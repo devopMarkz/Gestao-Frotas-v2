d04314c0477b06a45aff33adb65645e2f4fd8bc0
devopMarkz <marcosdev2002@gmail.com>
2025-02-11 19:41:14 -0300

    first commit

---
+++ src/main/java/com/github/devopMarkz/gestao_frotas/GestaoFrotasApplication.java
@@ -0,0 +1,13 @@
+package com.github.devopMarkz.gestao_frotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoFrotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoFrotasApplication.class, args);
+	}
+
+}
