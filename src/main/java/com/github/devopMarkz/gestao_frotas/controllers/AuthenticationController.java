42d13b8b74c8fcf32732ba434f582ab28bebd860
devopMarkz <marcosdev2002@gmail.com>
2025-02-13 13:22:09 -0300

    Documentando endpoints de Authentication Controller

--- src/main/java/com/github/devopMarkz/gestao_frotas/controllers/AuthenticationController.java
+++ src/main/java/com/github/devopMarkz/gestao_frotas/controllers/AuthenticationController.java
@@ -2,6 +2,10 @@ package com.github.devopMarkz.gestao_frotas.controllers;
 
 import com.github.devopMarkz.gestao_frotas.dtos.AuthDTO;
 import com.github.devopMarkz.gestao_frotas.services.TokenServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
+@Tag(name = "Autenticar Usuário", description = "Endpoint para autenticar usuário e receber Token JWT")
 public class AuthenticationController {
 
     private final AuthenticationManager authenticationManager;
@@ -23,6 +28,11 @@ public class AuthenticationController {
     }
 
     @PostMapping("/login")
+    @Operation(summary = "Endpoint de autenticação", description = "Autentica usuário")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Usuário autenticado."),
+            @ApiResponse(responseCode = "403", description = "Usuário não autenticado.")
+    })
     public String autenticarUsuario(@RequestBody AuthDTO authDTO){
         var authentication = new UsernamePasswordAuthenticationToken(authDTO.email(), authDTO.senha());
         authenticationManager.authenticate(authentication);
